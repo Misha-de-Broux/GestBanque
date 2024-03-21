7ab13ba0595efff68a08ba589f09af8bff2d410a
Misha de Broux <misha.debroux@gmail.com>
2024-03-21 16:01:20 +0100

    ex01 done

---
+++ Models/Current.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models {
+    internal class Current {
+
+        private double _maxCredit;
+        public string Number { get; set; }
+        public double Balance { get; private set; }
+        public double MaxCredit {
+            get { return _maxCredit; }
+            private set { if(value >=0) _maxCredit = value; }
+        }
+        public Person Owner { get; set; }
+
+        public void Deposit(double amount) { 
+            if (amount > 0) {
+                Balance += amount;
+            }
+        }
+
+        public void Withdraw(double amount) {
+            if(amount + MaxCredit > Balance) {
+                Balance -= amount;
+            }
+        }
+    }
+}
